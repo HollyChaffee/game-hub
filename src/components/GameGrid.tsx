0eee13d222787988f14dd94dab312b069f8f0557
Student 3 <hollydchaffee@gmail.com>
2023-06-19 10:25:42 -0400

    create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-import { Text } from '@chakra-ui/react';
+import { Text } from "@chakra-ui/react";
+import useGames from "./hooks/useGames";
 
-interface Game {
-    id: number;
-    name: string;
-}
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[]
-}
 /* need a state variable for storing game objects*/
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]); /* get games array and function to update it*/
-    const [error, setError] = useState(''); /*state variable for error messages*/
-    /* use effect hook to send fetch request to backend. Pass a callback, in this function need to call API client*/
-
-    useEffect(() => {
-        apiClient.get<FetchGamesResponse>('/xgames')
-            .then(res => setGames(res.data.results)) /*use an interface that represents the shape of the response object*/
-            .catch(err => setError(err.message));
-    })
+  const { games, error } =
+    useGames(); /* use object to destructure games and error. Now this component is primarily responsible for returning some markup */
   return (
     <>
-    {error && <Text>{error}</Text>}
-    <ul>
-        {games.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
---
+++ src/components/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[]
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]); /* get games array and function to update it*/
+    const [error, setError] = useState(''); /*state variable for error messages*/
+    /* use effect hook to send fetch request to backend. Pass a callback, in this function need to call API client*/
+
+    useEffect(() => {
+        const controller = new AbortController(); /* controller object to handle cancellations */
+
+        apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal})
+            .then((res) => setGames(res.data.results)) /*use an interface that represents the shape of the response object*/
+            .catch((err) => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)
+            });
+
+            return () => controller.abort(); /*return cleanup function*/
+    }, []); /*include array of dependencies in effect hook. Without this we cannot send a send request to backend.*/
+
+    return { games, error };
+}
+
+export default useGames;
