ef63fa94d4c0da573a01398ece74d36c76e43793
hollychaffee <saltdc95@gmail.com>
2023-07-24 13:35:49 -0400

    fixed skeletons not showing, removed unused imports

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,7 +11,6 @@ import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
-import { chakra } from "@chakra-ui/react";
 import { IconType } from "react-icons/lib";
 
 interface Props {
