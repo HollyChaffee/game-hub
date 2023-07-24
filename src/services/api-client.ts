8ee2ed5c84dc54a9f47e79bacc45b61f5e6ff107
hollychaffee <saltdc95@gmail.com>
2023-07-23 21:21:48 -0400

    updated title, added to gitignore, fixed paths

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-{/*create axios instance with custom congig*/}
+{/*create axios instance with custom config*/}
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {  /* with this config, the key will be included in the query string of every http request we send to backend*/ 
       key: '31cfafd31f9945519c82b453dac6b9fa'
-    }
-})
+    },
+});
